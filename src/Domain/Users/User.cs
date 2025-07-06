9ef89d57aecd48873b79feb825fc5fb374251fcb
Hikashi <hikashimlbb@gmail.com>
2025-07-06 12:20:27 +0700

    Added UserName Value Object to User entity

--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -4,6 +4,6 @@ public class User
 {
     public UserId Id { get; set; } = null!;
     public UserEmail Email { get; set; } = null!;
-    public string Name { get; set; } = null!;
+    public UserName Name { get; set; } = null!;
     public string Password { get; set; } = null!;
 }
