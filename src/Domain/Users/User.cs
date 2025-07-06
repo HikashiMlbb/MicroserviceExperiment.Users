baed94869421c2ed3021d11fd960eb35d4c9eaf6
Hikashi <hikashimlbb@gmail.com>
2025-07-06 12:18:21 +0700

    Added UserEmail Value Object to User entity

--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -3,7 +3,7 @@ namespace Domain.Users;
 public class User
 {
     public UserId Id { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public UserEmail Email { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string Password { get; set; } = null!;
 }
