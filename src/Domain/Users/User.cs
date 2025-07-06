e509cddb324657d08ac9e235cd4796a66fb4d5bf
Hikashi <hikashimlbb@gmail.com>
2025-07-06 12:16:13 +0700

    Added UserId Value Object to User entity

--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -2,7 +2,7 @@ namespace Domain.Users;
 
 public class User
 {
-    public int Id { get; set; }
+    public UserId Id { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string Password { get; set; } = null!;
