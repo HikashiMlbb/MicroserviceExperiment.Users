fc319be9625448f0ddf65a2458d4a39d4a254b21
Hikashi <hikashimlbb@gmail.com>
2025-07-06 12:34:28 +0700

    Added constructor to User entity

--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -1,9 +1,9 @@
 namespace Domain.Users;
 
-public sealed class User
+public sealed class User(UserId id, UserEmail email, UserName name, UserPassword password)
 {
-    public UserId Id { get; set; } = null!;
-    public UserEmail Email { get; set; } = null!;
-    public UserName Name { get; set; } = null!;
-    public UserPassword Password { get; set; } = null!;
+    public UserId Id { get; set; } = id;
+    public UserEmail Email { get; set; } = email;
+    public UserName Name { get; set; } = name;
+    public UserPassword Password { get; set; } = password;
 }
