ea2ea844d6251a65ac591f7954b7dbd889a42605
Hikashi <hikashimlbb@gmail.com>
2025-07-06 13:45:23 +0700

    Implemented User Email Value Object creating

--- src/Domain/Users/UserEmail.cs
+++ src/Domain/Users/UserEmail.cs
@@ -1,3 +1,31 @@
+using System.Text.RegularExpressions;
+using SharedKernel;
+
 namespace Domain.Users;
 
-public sealed record UserEmail(string Value);
+public sealed partial record UserEmail
+{
+    public string Value { get; init; }
+    private UserEmail(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<UserEmail> Create(string value)
+    {
+        var isMatch = MyRegex().IsMatch(value);
+
+        return !isMatch
+            ? UserErrors.EmailIsInvalid
+            : new UserEmail(value);
+    }
+
+    #region Private implementation
+
+    private const string EmailRegex = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+
+    [GeneratedRegex(EmailRegex)]
+    private static partial Regex MyRegex();
+
+    #endregion
+}
