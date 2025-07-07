72ae8ebea7a9fd632e7a503a0e9c1f328832b9b0
HikashiMlbb <130889711+HikashiMlbb@users.noreply.github.com>
2025-07-07 22:03:25 +0700

    Revert "Replaced AlreadyExists Error to separated class"
    This reverts commit 9733d45d495f77e843af1ab6118f8fdec30c109d.

--- src/Application/Users/UserErrors.cs
+++
@@ -1,8 +0,0 @@
-using SharedKernel;
-
-namespace Application.Users;
-
-public static class UserErrors
-{
-    public static Error AlreadyExists { get; } = new("User.AlreadyExists", "User with given Email or Username already exists."); 
-}
