d8b309a6aedf50c962c9b773533e7b29a5e0b882
Hikashi <hikashimlbb@gmail.com>
2025-07-08 20:35:11 +0700

    Added User Application Errors to separated class

--- src/Application/Users/UserErrors.cs
+++ src/Application/Users/UserErrors.cs
@@ -4,5 +4,7 @@ namespace Application.Users;
 
 public static class UserErrors
 {
-    public static Error AlreadyExists { get; } = new("User.AlreadyExists", "User with given Email or Username already exists."); 
+    public static Error AlreadyExists { get; } = new("User.AlreadyExists", "User with given Email or Username already exists.");
+    public static Error NotFound { get; } = new("User.NotFound", "User with given Username has not been found.");
+    public static Error LoginFailed { get; } = new("User.LoginFailed", "The entered password is incorrect.");
 }
