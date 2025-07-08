f9f0f63aa4f687a75748905b0d9233a2ddffe184
Hikashi <hikashimlbb@gmail.com>
2025-07-08 20:57:31 +0700

    Added IncorrectPassword Unit test to Sign In Use Case

--- tests/ApplicationTests/Users/SignInTests.cs
+++ tests/ApplicationTests/Users/SignInTests.cs
@@ -48,4 +48,30 @@ public class SignInTests
         _repoMock.Verify(x => x.Fetch(It.IsAny<UserName>()), Times.Once);
         _passwordMock.Verify(x => x.Verify(It.IsAny<UserRawPassword>(), It.IsAny<UserPassword>()), Times.Never);
     }
+    
+    [Test]
+    public async Task PasswordIsIncorrect_ReturnsError()
+    {
+        // Arrange
+        const string username = "SomeUsername";
+        const string password = "SomePassword";
+        var dto = new UserSignIn
+        {
+            Username = username,
+            Password = password
+        };
+        var user = new User(new UserId(15), new UserEmail("someemail@mail.com"), new UserName(username), new UserPassword(password));
+        _repoMock.Setup(x => x.Fetch(It.IsAny<UserName>())).ReturnsAsync(user);
+        _passwordMock.Setup(x => x.Verify(It.IsAny<UserRawPassword>(), It.IsAny<UserPassword>())).ReturnsAsync(false);
+        
+        // Act
+        var result = await _handler.Handle(dto);
+
+        // Assert
+        Assert.That(result.IsSuccess, Is.False);
+        Assert.That(result.Error!, Is.EqualTo(UserErrors.LoginFailed));
+        _repoMock.Verify(x => x.Fetch(It.IsAny<UserName>()), Times.Once);
+        _passwordMock.Verify(x => x.Verify(It.IsAny<UserRawPassword>(), It.IsAny<UserPassword>()), Times.Once);
+        _tokenMock.Verify(x => x.GenerateToken(It.IsAny<AuthorizationTokenPayload>()), Times.Never);
+    }
 }
