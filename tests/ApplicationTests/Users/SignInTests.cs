0df83b41bd72970b478d51a0114b48e6f4f29df1
Hikashi <hikashimlbb@gmail.com>
2025-07-08 20:54:37 +0700

    Added NotFound Unit test to Sign In Use Case

--- tests/ApplicationTests/Users/SignInTests.cs
+++ tests/ApplicationTests/Users/SignInTests.cs
@@ -5,6 +5,7 @@ using Domain.Users;
 using Moq;
 using NUnit.Framework;
 using UserDomainErrors = Domain.Users.UserErrors;
+using UserErrors = Application.Users.UserErrors;
 
 namespace ApplicationTests.Users;
 
@@ -23,5 +24,28 @@ public class SignInTests
         _repoMock = new Mock<IUserRepository>();
         _tokenMock = new Mock<IAuthorizationTokenService>();
         _handler = new UserSignInHandler(_passwordMock.Object, _repoMock.Object, _tokenMock.Object);
+    }
+
+    [Test]
+    public async Task NotFound_ReturnsError()
+    {
+        // Arrange
+        const string username = "SomeUsername";
+        const string password = "SomePassword";
+        var dto = new UserSignIn
+        {
+            Username = username,
+            Password = password
+        };
+        _repoMock.Setup(x => x.Fetch(It.IsAny<UserName>())).ReturnsAsync((User)null!);
+
+        // Act
+        var result = await _handler.Handle(dto);
+
+        // Assert
+        Assert.That(result.IsSuccess, Is.False);
+        Assert.That(result.Error!, Is.EqualTo(UserErrors.NotFound));
+        _repoMock.Verify(x => x.Fetch(It.IsAny<UserName>()), Times.Once);
+        _passwordMock.Verify(x => x.Verify(It.IsAny<UserRawPassword>(), It.IsAny<UserPassword>()), Times.Never);
     }
 }
