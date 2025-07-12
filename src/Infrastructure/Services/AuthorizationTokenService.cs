57daf2d3e93c748a694658f77412a9f524114a98
Hikashi <hikashimlbb@gmail.com>
2025-07-12 15:12:38 +0700

    Implemented JWT Generation

--- src/Infrastructure/Services/AuthorizationTokenService.cs
+++ src/Infrastructure/Services/AuthorizationTokenService.cs
@@ -1,11 +1,24 @@
+using System.Security.Claims;
 using Application.Abstractions;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Services;
 
-public class AuthorizationTokenService : IAuthorizationTokenService
+public class AuthorizationTokenService(AuthorizationTokenDescription tokenDescription) : IAuthorizationTokenService
 {
     public Task<string> GenerateToken(AuthorizationTokenPayload payload)
     {
-        throw new NotImplementedException();
+        var claims = new List<Claim> { new(JwtRegisteredClaimNames.Sub, payload.Id.Value.ToString()) };
+        var token = new SecurityTokenDescriptor
+        {
+            Issuer = tokenDescription.Issuer,
+            Audience = tokenDescription.Audience,
+            Expires = DateTime.UtcNow + tokenDescription.Expiration,
+            Subject = new ClaimsIdentity(claims),
+            SigningCredentials = new SigningCredentials(tokenDescription.Key, SecurityAlgorithms.HmacSha384)
+        };
+
+        return Task.FromResult(new JsonWebTokenHandler().CreateToken(token));
     }
 }
