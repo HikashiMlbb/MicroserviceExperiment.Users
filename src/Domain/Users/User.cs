ebd1013b70bb2cdf8bd6b97d5abbe7be606dbc5e
Hikashi <hikashimlbb@gmail.com>
2025-07-13 22:57:50 +0700

    Fixed User Entity constructors for ORM

--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -1,9 +1,23 @@
 namespace Domain.Users;
 
-public sealed class User(UserId id, UserEmail email, UserName name, UserPassword password)
+public sealed class User
 {
-    public UserId Id { get; set; } = id;
-    public UserEmail Email { get; set; } = email;
-    public UserName Name { get; set; } = name;
-    public UserPassword Password { get; set; } = password;
+    public UserId Id { get; set; } = null!;
+    public UserEmail Email { get; set; } = null!;
+    public UserName Name { get; set; } = null!;
+    public UserPassword Password { get; set; } = null!;
+
+    public User(UserId id, UserEmail email, UserName name, UserPassword password)
+    {
+        Id = id;
+        Email = email;
+        Name = name;
+        Password = password;
+    }
+    
+    // Needed by ORM
+    private User()
+    {
+        
+    }
 }
