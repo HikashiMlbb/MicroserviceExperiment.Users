b2dd74e67f951ffbb701b927a3e050148ba425b9
Hikashi <hikashimlbb@gmail.com>
2025-08-08 15:25:29 +0700

    Refactored UserErrors.cs to UserApplicationErrors.cs

--- src/Application/Users/UserErrors.cs
+++ src/Application/Users/UserErrors.cs
@@ -2,9 +2,11 @@ using SharedKernel;
 
 namespace Application.Users;
 
-public static class UserErrors
+public record UserApplicationError(string? Code = null, string? Message = null) : Error(Code, Message);
+
+public static class UserApplicationErrors
 {
-    public static Error AlreadyExists { get; } = new("User.AlreadyExists", "User with given Email or Username already exists.");
-    public static Error NotFound { get; } = new("User.NotFound", "User with given Username has not been found.");
-    public static Error LoginFailed { get; } = new("User.LoginFailed", "The entered password is incorrect.");
+     public static UserApplicationError AlreadyExists { get; } = new("User.AlreadyExists", "User with given Email or Username already exists.");
+     public static UserApplicationError NotFound { get; } = new("User.NotFound", "User with given Username has not been found.");
+     public static UserApplicationError LoginFailed { get; } = new("User.LoginFailed", "The entered password is incorrect.");
 }
