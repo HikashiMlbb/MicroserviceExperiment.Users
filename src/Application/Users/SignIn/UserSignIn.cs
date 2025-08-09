e71aaca3f8b57243f58bb35820aefa8efa7424d6
Hikashi <hikashimlbb@gmail.com>
2025-08-09 16:18:22 +0700

    Applied nullable properties to User Sign-In DTO

--- src/Application/Users/SignIn/UserSignIn.cs
+++ src/Application/Users/SignIn/UserSignIn.cs
@@ -2,6 +2,6 @@ namespace Application.Users.SignIn;
 
 public class UserSignIn
 {
-    public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string? Username { get; set; }
+    public string? Password { get; set; }
 }
