c4a5e73c26719bfee37c83789946e819c1bd6371
Hikashi <hikashimlbb@gmail.com>
2025-08-11 15:52:45 +0700

    Implemented Sign-Up API Endpoint

--- src/API/Endpoints/UserEndpoints.cs
+++ src/API/Endpoints/UserEndpoints.cs
@@ -1,3 +1,9 @@
+using API.Contracts;
+using Application.Users;
+using Application.Users.SignUp;
+using Domain.Users;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Endpoints;
 
 public static class UserEndpoints
@@ -6,7 +12,33 @@ public static class UserEndpoints
     {
         var api = app.MapGroup($"{prefix}/users");
 
-        api.MapPost("/sign-up");
+        api.MapPost("/sign-up", SignUp);
         api.MapPost("/sign-in");
     }
+
+    #region Endpoint implementation
+
+    private static async Task<IResult> SignUp(
+        [FromBody] ApiSignUpContract contract,
+        [FromServices] UserSignUpHandler handler)
+    {
+        var dto = new UserSignUp
+        {
+            Email = contract.Email,
+            Username = contract.Username,
+            Password = contract.Password
+        };
+        var result = await handler.Handle(dto);
+
+        if (result.IsSuccess) return Results.Ok(result.Value);
+
+        return result.Error switch
+        {
+            UserDomainError domain => Results.BadRequest(new { domain.Code, domain.Message }),
+            UserApplicationError application => Results.Conflict(new { application.Code, application.Message }),
+            _ => Results.StatusCode(500)
+        };
+    }
+
+    #endregion
 }
