f762a2ccbb07944b857c3ca467901457486af6ad
Hikashi <hikashimlbb@gmail.com>
2025-10-11 21:28:44 +0700

    Added ResetTokenExpiration ValueObject

---
+++ src/Domain/ResetTokens/ResetTokenExpiration.cs
@@ -0,0 +1,27 @@
+using SharedKernel;
+
+namespace Domain.ResetTokens;
+
+public sealed record ResetTokenExpiration
+{
+    public DateTime Value { get; init; }
+
+    private static readonly TimeSpan MinExpiration = TimeSpan.FromMinutes(1);
+    private static readonly TimeSpan MaxExpiration = TimeSpan.FromDays(1);
+    
+    private ResetTokenExpiration(DateTime value)
+    {
+        Value = value;
+    }
+
+    public static Result<ResetTokenExpiration> Create(DateTime value)
+    {
+        var timespan = DateTime.UtcNow - value;
+        if (timespan < MinExpiration || timespan > MaxExpiration)
+        {
+            return ResetTokenDomainErrors.ExpirationOutOfRange;
+        }
+
+        return new ResetTokenExpiration(value);
+    }
+}
