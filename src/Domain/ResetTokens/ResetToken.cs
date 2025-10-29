0c030842b868058949afbbfe5b01a90989166af0
Hikashi <hikashimlbb@gmail.com>
2025-10-29 14:58:11 +0700

    Fixed Reset Token Entity Constructor

--- src/Domain/ResetTokens/ResetToken.cs
+++ src/Domain/ResetTokens/ResetToken.cs
@@ -7,4 +7,11 @@ public sealed class ResetToken
     public UserEmail Email { get; set; } = null!;
     public ResetTokenValue Token { get; set; } = null!;
     public ResetTokenExpiration Expiration { get; set; } = null!;
+
+    public ResetToken(UserEmail email, ResetTokenValue value, ResetTokenExpiration expiration)
+    {
+        Email = email;
+        Token = value;
+        Expiration = expiration;
+    }
 }
