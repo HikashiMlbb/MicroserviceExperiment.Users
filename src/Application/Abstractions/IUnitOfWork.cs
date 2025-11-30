8a0aa66bb9805c38b2015c2415246dc757916eb8
Hikashi <hikashimlbb@gmail.com>
2025-11-30 14:17:22 +0700

    Relocated and Updated IUnitOfWork interface

--- src/Application/Abstractions/IUnitOfWork.cs
+++ src/Application/Abstractions/IUnitOfWork.cs
@@ -1,8 +1,12 @@
-namespace Application.Users;
+using Application.ResetTokens;
+using Application.Users;
+
+namespace Application.Abstractions;
 
 public interface IUnitOfWork : IDisposable
 {
     public IUserRepository Users { get; }
+    public IResetTokenRepository ResetTokens { get; }
     
     public Task Commit();
     public Task Rollback();
